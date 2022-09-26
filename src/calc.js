b33a1a8ab6466e6b694a9d3a081063e509a10dfc
TylerS2000 <tylerschulten@gmail.com>
2022-09-26 18:43:52 -0400

    bug fixes, and added protein modifier.

--- src/calc.js
+++ src/calc.js
@@ -7,7 +7,10 @@ export default function Calc(){
     const[final, setFinal]=React.useState("")
     const[final2, setFinal2]=React.useState("")
     const[final3, setFinal3]=React.useState("")
-    function handleClick(){
+    const[proteinMod, setproteinMod]=React.useState("")
+   
+    
+    function handleClick(){  
         let BMI = wtVal/((htVal*2.54/100)*(htVal*2.54/100))
         let range1 
         let range2
@@ -16,29 +19,34 @@ export default function Calc(){
         let kcal1
         let kcal2
         let idealWtVal 
-        let pro1
-        let pro2
+        let pro1 = 0 
+        let pro2 = 0 
         let gen = genderValue.toLowerCase();
         let bw = "IBW"
         
-
+       
+        if(gen==="m"){idealWtVal = 50 + 1.9*(wtVal-60);}
+        else if(gen==="f"){idealWtVal = 50 + 1.7*(wtVal-60);}
         if(BMI<19){kcal1 =30; kcal2=35;range1 = kcal1 * wtVal; range2 = kcal2 * wtVal;}
             else if(BMI>=19 && BMI <= 25){kcal1 =25; kcal2=30;range1 = kcal1 * wtVal; range2 = kcal2 * wtVal;}
                 else if(BMI>25 && BMI <30){kcal1 =20; kcal2=25;range1 = kcal1 * wtVal; range2 = kcal2 * wtVal;}
                     else if(BMI>=30 && BMI <40){kcal1 =15; kcal2=18;range1 = kcal1 * wtVal; range2 = kcal2 * wtVal;}
                             else if(BMI>=40 && BMI <50){kcal1 =8; kcal2=15;range1 = kcal1 * wtVal; range2 = kcal2 * wtVal;}
                                 else if(BMI >= 50){kcal1 =8; kcal2=11;range1 = kcal1 * wtVal; range2 = kcal2 * wtVal;}
-       setFinal(`${range1}-${range2}kcals(${kcal1}-${kcal2}kcals/kg ${wtVal}kg ABW)`)
+       setFinal(`${Math.round(range1)}-${Math.round(range2)}kcals(${kcal1}-${kcal2}kcals/kg ${wtVal}kg ABW)`)
        
-       if(gen==="m"){idealWtVal = 50 + 1.9*(wtVal-60);}
-        if(gen==="f"){idealWtVal = 50 + 1.7*(wtVal-60);}
 
-       if(BMI<19){ bw ="ABW"; pro1 =1.4; pro2=1.6;range1p = pro1 * wtVal; range2p = pro2 * wtVal;}
-            else if(BMI>=19 && BMI <= 25){bw = "ABW"; pro1 =1.4; pro2=1.6;range1p = pro1 * wtVal; range2p = pro2 * wtVal;}
-                else if(BMI>25 && BMI <30){pro1 =1.4; pro2=1.6;range1p = pro1 * idealWtVal; range2p = pro2 * idealWtVal;}
-                    else if(BMI>=30 && BMI <40){pro1 =1.4; pro2=1.6;range1p = pro1 * idealWtVal; range2p = kcal2 * idealWtVal;}
-                            else if(BMI>=40 && BMI <50){pro1 =1.8; pro2=2;range1p = pro1 * idealWtVal; range2p = pro2 * idealWtVal;}
-                                else if(BMI >= 50){kcal1 =1.8; kcal2=2;range1p = kcal1 * idealWtVal; range2p = kcal2 * idealWtVal;}
+        if(proteinMod.length>0){pro1+=Number(proteinMod); pro2+=Number(proteinMod)}
+        
+       if(BMI<19){bw ="ABW"; pro1 +=1.4;; pro2+=1.6; range1p = pro1 * wtVal; range2p = pro2 * wtVal;}
+            else if(BMI>=19 && BMI <= 25){bw = "ABW"; pro1 += 1.0; pro2+=1.2;range1p = pro1 * wtVal; range2p = pro2 * wtVal;}
+                else if(BMI>25 && BMI <30){pro1+=1.4;  pro2+=1.6;range1p = pro1 * idealWtVal; range2p = pro2 * idealWtVal;}
+                    else if(BMI>=30 && BMI <40){pro1+=1.2; pro2+=1.4;range1p = pro1 * idealWtVal; range2p = kcal2 * idealWtVal;}
+                            else if(BMI>=40 && BMI <50){pro1+=1.8; pro2+=2;range1p = pro1 * idealWtVal; range2p = pro2 * idealWtVal;}
+                                else if(BMI >= 50){pro1+=1.8;pro1 = (Math.round(pro1 * 10) / 10 ); pro2+=2;range1p = pro1 * idealWtVal; range2p = pro2 * idealWtVal;}
+        
+        pro1 = (Math.round(pro1 * 10) / 10 );
+        pro2 = (Math.round(pro2 * 10) / 10 );                  
         setFinal2(`${Math.round(range1p)}-${Math.round(range2p)}g(${pro1}-${pro2}g/kg ${wtVal}kg ${bw})`)
         setFinal3(`${range1}-${range2}ml`)
     }
@@ -52,11 +60,18 @@ export default function Calc(){
             <input type= "text" className="input" id="height" placeholder="height in inches" value={htVal} onChange={(x)=>setHtVal(x.target.value) }/>
             <input type= "text" className="input" id="weight" placeholder="weight in kg" value={wtVal} onChange={(x)=>setWtVal(x.target.value) } />
             <input type= "text" className="input" id="gender" placeholder="gender m/f" value={genderValue} onChange={(x)=>setGenderVal(x.target.value) } />
-            <p></p>
+            <input type= "text" className="input" id="height" placeholder="optional protein mod" value={proteinMod} onChange={(x)=>setproteinMod(x.target.value)}/>
         </div>
+        <div className="check--container">
+        
+            
+        </div>
+
         <div className="button--container">
+        
         <button id = "submit" className="submit"  onClick={handleClick}>press to submit</button>
         </div>
+
         <div className="results--container">
         <div className="results">
             <h1>Kcal Needs</h1>
